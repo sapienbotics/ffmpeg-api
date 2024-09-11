a6599c7012ed9bedf98b78733a4547fd1668f42c
Saurabh Singh <saurabh.valetudeprimus.com>
2024-09-11 21:33:40 +0530

    Updated server.js to handle dynamic audio volume control and fix FFmpeg command

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
-const { exec } = require('child_process');
+const { exec, execSync } = require('child_process');
 const { v4: uuidv4 } = require('uuid');
+const ffprobe = require('ffprobe-static');
 const ffmpegPath = require('ffmpeg-static');
 
 const app = express();
@@ -16,24 +17,64 @@ if (!fs.existsSync(storageDir)) {
 }
 
 async function downloadFile(url, outputPath) {
-  const response = await axios.get(url, { responseType: 'stream' });
-  response.data.pipe(fs.createWriteStream(outputPath));
-  return new Promise((resolve, reject) => {
-    response.data.on('end', resolve);
-    response.data.on('error', reject);
-  });
+  let retries = 3;
+  while (retries > 0) {
+    try {
+      const response = await axios.get(url, { responseType: 'stream' });
+      response.data.pipe(fs.createWriteStream(outputPath));
+      return new Promise((resolve, reject) => {
+        response.data.on('end', resolve);
+        response.data.on('error', reject);
+      });
+    } catch (error) {
+      console.error('Error downloading file, retrying...', error.message);
+      retries--;
+      if (retries === 0) throw new Error('Failed to download file after retries');
+    }
+  }
 }
 
-function executeFFmpegCommand(command) {
+function logFileProperties(filePath) {
+  try {
+    const output = execSync(`${ffprobe.path} -v error -show_format -show_streams ${filePath}`).toString();
+    console.log(`File properties for ${filePath}:\n`, output);
+  } catch (error) {
+    console.error(`Error logging properties for ${filePath}:`, error.message);
+  }
+}
+
+function preprocessAudio(inputAudioPath, outputAudioPath, volume) {
   return new Promise((resolve, reject) => {
+    const command = `${ffmpegPath} -i ${inputAudioPath} -ar 44100 -ac 2 -filter:a "volume=${volume}" ${outputAudioPath}`;
     exec(command, (error, stdout, stderr) => {
       if (error) {
-        console.error('FFmpeg error:', error.message);
+        console.error('FFmpeg error during audio preprocessing:', error.message);
         console.error('FFmpeg stderr:', stderr);
         reject(error);
       } else {
-        console.log('FFmpeg stdout:', stdout);
-        resolve(stdout);
+        console.log('FFmpeg output during audio preprocessing:', stdout);
+        resolve();
+      }
+    });
+  });
+}
+
+function executeFFmpegCommand(inputVideoPath, inputAudioPath, backgroundAudioPath, outputPath, options) {
+  return new Promise((resolve, reject) => {
+    const command = `${ffmpegPath} -i ${inputVideoPath} -i ${inputAudioPath} -i ${backgroundAudioPath} ` +
+      `-filter_complex "[1:a]volume=${options.inputAudioVolume}[a1]; ` +
+      `[2:a]volume=${options.backgroundAudioVolume}[a2]; ` +
+      `[0:v][a1][a2]amerge=inputs=2[a]" ` +
+      `-map 0:v -map "[a]" -c:v libx264 -c:a aac -b:a 128k -ac 2 -ar 44100 -shortest -report ${outputPath}`;
+
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error('FFmpeg error during merging:', error.message);
+        console.error('FFmpeg stderr:', stderr);
+        reject(error);
+      } else {
+        console.log('FFmpeg output during merging:', stdout);
+        resolve();
       }
     });
   });
@@ -41,47 +82,66 @@ function executeFFmpegCommand(command) {
 
 app.post('/edit-video', async (req, res) => {
   try {
-    const { inputVideo, inputAudio, backgroundAudio, inputAudioVolume, backgroundAudioVolume, outputFile, options } = req.body;
-
+    console.log('Request received:', req.body);
+    const inputVideoUrl = req.body.inputVideo;
+    const inputAudioUrl = req.body.inputAudio;
+    const backgroundAudioUrl = req.body.backgroundAudio;
+    const volume = req.body.volume || '1';  // Default volume to 1 if not provided
     const uniqueFilename = `${uuidv4()}_processed_video.mp4`;
     const outputFilePath = path.join(storageDir, uniqueFilename);
     const tempVideoPath = path.join(storageDir, `${uuidv4()}_temp_video.mp4`);
     const tempAudioPath = path.join(storageDir, `${uuidv4()}_temp_audio.mp3`);
     const tempBackgroundAudioPath = path.join(storageDir, `${uuidv4()}_temp_background_audio.mp3`);
+    const processedAudioPath = path.join(storageDir, `${uuidv4()}_processed_audio.mp4`);
 
-    // Download files
-    console.log('Downloading input video...');
-    await downloadFile(inputVideo, tempVideoPath);
-    console.log('Downloading input audio...');
-    await downloadFile(inputAudio, tempAudioPath);
-    console.log('Downloading background audio...');
-    await downloadFile(backgroundAudio, tempBackgroundAudioPath);
+    console.log('Downloading video from:', inputVideoUrl);
+    await downloadFile(inputVideoUrl, tempVideoPath);
+    console.log('Downloading audio from:', inputAudioUrl);
+    await downloadFile(inputAudioUrl, tempAudioPath);
+    console.log('Downloading background audio from:', backgroundAudioUrl);
+    await downloadFile(backgroundAudioUrl, tempBackgroundAudioPath);
 
-    // Log file properties (optional)
-    // logFileProperties(tempVideoPath); 
+    logFileProperties(tempVideoPath);
+    logFileProperties(tempAudioPath);
+    logFileProperties(tempBackgroundAudioPath);
 
-    // FFmpeg command to merge video, audio, and background audio
-    const ffmpegCommand = `
-      ${ffmpegPath} -i ${tempVideoPath} -i ${tempAudioPath} -i ${tempBackgroundAudioPath} \
-      -filter_complex "[1:a]volume=${inputAudioVolume}[a1];[2:a]volume=${backgroundAudioVolume}[a2];[a1][a2]amerge=inputs=2,pan=stereo|c0<c0+c2|c1<c1+c3[a]" \
-      -map 0:v -map "[a]" ${options} ${outputFilePath}
-    `;
+    console.log('Preprocessing main audio...');
+    await preprocessAudio(tempAudioPath, processedAudioPath, volume);
 
-    console.log('Running FFmpeg command...');
-    console.log(ffmpegCommand);
+    console.log('Processing video with audio...');
+    const options = {
+      inputAudioVolume: req.body.inputAudioVolume || '1.0',
+      backgroundAudioVolume: req.body.backgroundAudioVolume || '0.0',
+    };
+    await executeFFmpegCommand(tempVideoPath, processedAudioPath, tempBackgroundAudioPath, outputFilePath, options);
 
-    await executeFFmpegCommand(ffmpegCommand);
-
-    // Clean up temporary files
-    fs.unlink(tempVideoPath, err => { if (err) console.error('Error deleting temp video file:', err.message); });
-    fs.unlink(tempAudioPath, err => { if (err) console.error('Error deleting temp audio file:', err.message); });
-    fs.unlink(tempBackgroundAudioPath, err => { if (err) console.error('Error deleting temp background audio file:', err.message); });
+    fs.unlink(tempVideoPath, (err) => {
+      if (err) console.error('Error deleting temp video file:', err.message);
+    });
+    fs.unlink(tempAudioPath, (err) => {
+      if (err) console.error('Error deleting temp audio file:', err.message);
+    });
+    fs.unlink(tempBackgroundAudioPath, (err) => {
+      if (err) console.error('Error deleting temp background audio file:', err.message);
+    });
+    fs.unlink(processedAudioPath, (err) => {
+      if (err) console.error('Error deleting processed audio file:', err.message);
+    });
 
     res.json({ message: 'Video processed successfully', outputFile: uniqueFilename });
-
   } catch (error) {
     console.error('Error processing video:', error.message);
     res.status(500).json({ error: 'Error processing video' });
+  }
+});
+
+app.get('/video/:filename', (req, res) => {
+  const filePath = path.join(storageDir, req.params.filename);
+
+  if (fs.existsSync(filePath)) {
+    res.sendFile(filePath);
+  } else {
+    res.status(404).send('File not found');
   }
 });
 
