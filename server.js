e0874005e5132259c04faf576bc85809afc934e6
Saurabh Singh <saurabh.valetudeprimus.com>
2024-12-29 03:58:29 +0530

    nbsvdhkfl

--- server.js
+++ server.js
@@ -298,8 +298,8 @@ const effects = [
     // Stationary Effect (Centered with padding)
     //`scale=${width}:${height}:force_original_aspect_ratio=decrease,pad=${width}:${height}:(ow-iw)/2:(oh-ih)/2:color=${dominantColor}`,
 
-    // Slow Zoom In Effect (Stops at 1.2x zoom)
-    //`zoompan=z='if(lte(zoom,1.2),zoom+0.0015,zoom)':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':d=${duration * 30}:s=${width}x${height}`,
+    // Slow Zoom In Effect (Stops at 1.2x zoom) with Pixel Movement
+    `zoompan=z='if(lte(zoom,1.2),zoom+0.0015,zoom)':x='if(gte(on,1),x+1,x)':y='if(gte(on,1),ih/2-(ih/zoom/2)+(x/iw)*0.25,ih/2-(ih/zoom/2))':d=${duration * 30}:s=${width}x${height}`,
 
     // Slow Zoom Out Effect (Stops at 1x zoom)
    // `zoompan=z='if(eq(on,0),1.2,if(gte(zoom,1),zoom-0.0015,zoom))':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':d=${duration * 30},scale=${width}:${height}:force_original_aspect_ratio=decrease,pad=${width}:${height}:(ow-iw)/2:(oh-ih)/2:color=${dominantColor}`,
@@ -308,8 +308,7 @@ const effects = [
    // `fade=in:0:30,fade=out:${duration * 30 - 30}:30,scale=${width}:${height}:force_original_aspect_ratio=decrease,pad=${width}:${height}:(ow-iw)/2:(oh-ih)/2:color=${dominantColor}`,
 
 // Ken Burns Effect (Zoom with subtle pan left movement)
-// Ken Burns Effect (Zoom with subtle pan left movement)
-`zoompan=z='if(gte(on,1),zoom+0.0005,zoom)':x='if(gte(on,1),x+1,x)':y='if(gte(on,1),ih/2-(ih/zoom/2)+(x/iw)*0.25,ih/2-(ih/zoom/2))':d=${duration * 30}:s=${width}x${height},scale='if(gte(iw/ih,${width}/${height}),${width}:-1,-1,${height})':force_original_aspect_ratio=decrease,pad=${width}:${height}:(ow-iw)/2:(oh-ih)/2:color=${dominantColor}`,
+//`zoompan=z='if(gte(on,1),zoom+0.0005,zoom)':x='if(gte(on,1),x+1,x)':y='if(gte(on,1),ih/2-(ih/zoom/2)+(x/iw)*0.25,ih/2-(ih/zoom/2))':d=${duration * 30}:s=${width}x${height},scale='if(gte(iw/ih,${width}/${height}),${width}:-1,-1,${height})':force_original_aspect_ratio=decrease,pad=${width}:${height}:(ow-iw)/2:(oh-ih)/2:color=${dominantColor}`,
 
     // Ken Burns Effect (Zoom with subtle pan right movement)
     //`zoompan=z='if(gte(on,1),zoom+0.0005,zoom)':x='if(gte(on,1),x-3,x)':y='ih/2-(ih/zoom/2)':d=${duration * 30}:s=${width}x${height},scale=${width}:${height}:force_original_aspect_ratio=decrease,pad=${width}:${height}:(ow-iw)/2:(oh-ih)/2:color=${dominantColor}`,
