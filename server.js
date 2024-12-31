222a349da276a797c35739b34191637725936688
Saurabh Singh <saurabh.valetudeprimus.com>
2024-12-31 14:33:55 +0530

    nihuhqbh

--- server.js
+++ server.js
@@ -302,7 +302,11 @@ const effects = [
     //`zoompan=z='if(lte(zoom,1.2),zoom+0.0015,zoom)':x='if(gte(on,1),x+1,x)':y='if(gte(on,1),ih/2-(ih/zoom/2)+(x/iw)*0.25,ih/2-(ih/zoom/2))':d=${duration * 30}:s=${width}x${height}`,
 
     //Slow Zoom Out Effect (Stops at 1x zoom) with Pixel Movement
-   `zoompan=z='if(eq(on,0),1.2,if(gte(zoom,1),zoom-0.0005,zoom))':x='if(gte(on,1),x+1,x)':y='if(gte(on,1),ih/2-(ih/zoom/2)+(x/iw)*0.25,ih/2-(ih/zoom/2))':d=${duration * 30},scale=${width}:${height}:force_original_aspect_ratio=decrease,pad=${width}:${height}:(ow-iw)/2:(oh-ih)/2:color=${dominantColor}`,
+  // `zoompan=z='if(eq(on,0),1.2,if(gte(zoom,1),zoom-0.0005,zoom))':x='if(gte(on,1),x+1,x)':y='if(gte(on,1),ih/2-(ih/zoom/2)+(x/iw)*0.25,ih/2-(ih/zoom/2))':d=${duration * 30},scale=${width}:${height}:force_original_aspect_ratio=decrease,pad=${width}:${height}:(ow-iw)/2:(oh-ih)/2:color=${dominantColor}`,
+
+  //Slow Zoom Out Effect (Stops at 1x zoom) new trial
+   `zoompan=z='if(eq(on,0),1.2,if(gte(zoom,1),zoom-0.0005,zoom))':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':d=${duration * 30},scale='if(gt(a,${width}/${height}),${width}:-1,-1:${height})':force_original_aspect_ratio=decrease,pad=${width}:${height}:(ow-iw)/2:(oh-ih)/2:color=${dominantColor}`,
+
 
     // Fade-in and Fade-out Effect (Smooth fade transition)
    // `fade=in:0:30,fade=out:${duration * 30 - 30}:30,scale=${width}:${height}:force_original_aspect_ratio=decrease,pad=${width}:${height}:(ow-iw)/2:(oh-ih)/2:color=${dominantColor}`,
